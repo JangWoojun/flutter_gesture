2907460f5df6cbd87d5d8d6ff5bf19c233bf475a
JangWoojun <woojun0107@naver.com>
2024-01-23 15:12:45 +0900

    RadioButton

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,77 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Column(
-            children: [
-              TestCheckBox(),
-            ],
+            children: [TestCheckBox(), TestRadioButton()],
           ),
         ),
       ),
+    );
+  }
+}
+
+enum TestRadioValue {
+  test1,
+  test2,
+  test3;
+}
+
+class TestRadioButton extends StatefulWidget {
+  const TestRadioButton({super.key});
+
+  @override
+  State<TestRadioButton> createState() => _TestRadioButtonState();
+}
+
+class _TestRadioButtonState extends State<TestRadioButton> {
+  TestRadioValue? selectValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          title: Text(TestRadioValue.test1.name),
+          onTap: () {
+            setState(() {
+              if (selectValue != TestRadioValue.test1) {
+                selectValue = TestRadioValue.test1;
+              }
+            });
+          },
+          leading: Radio<TestRadioValue>(
+              value: TestRadioValue.test1,
+              groupValue: selectValue,
+              onChanged: (value) => setState(() => selectValue = value!)),
+        ),
+        ListTile(
+          title: Text(TestRadioValue.test2.name),
+          onTap: () {
+            setState(() {
+              if (selectValue != TestRadioValue.test2) {
+                selectValue = TestRadioValue.test2;
+              }
+            });
+          },
+          leading: Radio<TestRadioValue>(
+              value: TestRadioValue.test2,
+              groupValue: selectValue,
+              onChanged: (value) => setState(() => selectValue = value!)),
+        ),
+        ListTile(
+          title: Text(TestRadioValue.test3.name),
+          onTap: () {
+            setState(() {
+              if (selectValue != TestRadioValue.test3) {
+                selectValue = TestRadioValue.test3;
+              }
+            });
+          },
+          leading: Radio<TestRadioValue>(
+              value: TestRadioValue.test3,
+              groupValue: selectValue,
+              onChanged: (value) => setState(() => selectValue = value!)),
+        ),
+      ],
     );
   }
 }
