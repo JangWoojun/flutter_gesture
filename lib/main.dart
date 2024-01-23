b8121f0d53e202abebf75ff7a6ccc6f4bde1d6de
JangWoojun <woojun0107@naver.com>
2024-01-23 14:46:56 +0900

    CheckBox

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,64 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              TestCheckBox(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TestCheckBox extends StatefulWidget {
+  const TestCheckBox({super.key});
+
+  @override
+  State<TestCheckBox> createState() => _TestCheckBoxState();
+}
+
+class _TestCheckBoxState extends State<TestCheckBox> {
+  late List<bool> values;
+
+  @override
+  void initState() {
+    super.initState();
+    values = [false, false, false];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Checkbox(
+            value: values[0],
+            onChanged: (value) => changeValue(0, value: value)),
+        Checkbox(
+            value: values[1],
+            onChanged: (value) => changeValue(1, value: value)),
+        Checkbox(
+            value: values[2],
+            onChanged: (value) => changeValue(2, value: value)),
+      ],
+    );
+  }
+
+  void changeValue(int index, {bool? value = false}) {
+    setState(() {
+      values[index] = value!;
+    });
+  }
 }
